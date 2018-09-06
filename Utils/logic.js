cbd7be1244f357d44874ad9b62ac59fa4faf5800
Steven <Admin@wuk3029841.ds.wolseley.com>
2018-09-06 12:05:30 +0100

    added a new file structure to the app

--- Utils/logic.js
+++ Utils/logic.js
@@ -2,22 +2,26 @@
 const xlsx = require('node-xlsx').default,
       fs = require("fs");
 
+// Import local
+const {Success} = require("./successMessage.js");
+
 // Import Models
 const {DealsCat} = require("./Models/DealsCat.js"),
       {DealsHP} = require("./Models/DealsHomepage.js");
 
-let data_model;
+let data_model = null,
+    finalCount = 0;
 
 const Logic = (input) => {
 
-  data_model = xlsx.parse(`./Data/deals_data_model.xlsx`);
+  data_model = xlsx.parse(`Utils/Data/data_model.xlsx`);
 
   let dataArr = data_model[0].data,
       count = 0;
 
   for(let i = 1; i < dataArr.length; i++) {
     count++;
-    fs.writeFileSync(`../Banners/${count}-homepage_banner_${dataArr[i][0]}.txt`, DealsHP(
+    fs.writeFileSync(`Banners/${count}-homepage_banner_${dataArr[i][0]}.txt`, DealsHP(
       dataArr[i][0],
       dataArr[i][1],
       dataArr[i][2],
@@ -32,12 +36,14 @@ const Logic = (input) => {
       dataArr[i][11],
       dataArr[i][12],
       dataArr[i][13]
-
     ));
   }
-
+  Success(count);
 }
 
+
+
 module.exports = {
-  Logic
+  Logic,
+  finalCount
 }
--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 
 // Import local
-const {Success} = require("./Utils/successMessage.js"),
-      {Compress} = require("./Utils/compress.js"),
-      {Logic} = require("./Utils/logic.js");
-
+const {Compress} = require("./Utils/compress.js"),
+      {Logic,finalCount} = require("./Utils/logic.js");
 
 Logic();
 
 
 Compress('Banners');
-Success(count);
