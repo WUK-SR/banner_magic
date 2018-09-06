1b45f314e6cc46d6f7954d8ae4cba68bf0bc1c31
Steven <Admin@wuk3029841.ds.wolseley.com>
2018-09-06 11:45:38 +0100

    amended logic

---
+++ Utils/logic.js
@@ -0,0 +1,42 @@
+// Import third-party
+const xlsx = require('node-xlsx').default,
+      fs = require("fs");
+
+// Import Models
+const {DealsCat} = require("./Models/DealsCat.js"),
+      {DealsHP} = require("./Models/DealsHomepage.js");
+
+let data_model;
+
+const Logic = (input) => {
+
+  data_model = xlsx.parse(`./Data/deals_data_model.xlsx`);
+
+  let dataArr = data_model[0].data,
+      count = 0;
+
+  for(let i = 1; i < dataArr.length; i++) {
+    count++;
+    fs.writeFileSync(`../Banners/${count}-homepage_banner_${dataArr[i][0]}.txt`, DealsHP(
+      dataArr[i][0],
+      dataArr[i][1],
+      dataArr[i][2],
+      dataArr[i][3],
+      dataArr[i][4],
+      dataArr[i][5],
+      dataArr[i][6],
+      dataArr[i][7],
+      dataArr[i][8],
+      dataArr[i][9],
+      dataArr[i][10],
+      dataArr[i][11],
+      dataArr[i][12],
+      dataArr[i][13]
+
+    ));
+  }
+}
+
+module.exports = {
+  Logic
+}
