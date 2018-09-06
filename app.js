7be47433f1a37ed1e61575732cae2699e21af087
Steven <Admin@wuk3029841.ds.wolseley.com>
2018-09-06 10:57:00 +0100

    added a deals_cat_banner model

--- app.js
+++ app.js
@@ -6,24 +6,34 @@ const xlsx = require('node-xlsx').default,
 // Import local
 const {Success} = require("./Utils/successMessage.js"),
       {Model} = require("./Utils/models.js"),
+      {DealsCat} = require("./Utils/Models/DealsCat.js"),
       {Compress} = require("./Utils/compress.js");
 
 
-let data_model = xlsx.parse(`Utils/Data/data_model.xlsx`),
+let data_model = xlsx.parse(`Utils/Data/deals_cat_data_model.xlsx`),
     dataArr = data_model[0].data,
     count = 0;
 
-for(let i = 0; i < dataArr.length; i++) {
+for(let i = 1; i < dataArr.length; i++) {
   count++;
-  fs.writeFileSync(`Banners/${count}-${dataArr[i][0]}.txt`, Model(
+  fs.writeFileSync(`Banners/${count}-category_banner_${dataArr[i][0]}.txt`, DealsCat(
     dataArr[i][0],
     dataArr[i][1],
-    dataArr[i][2]
+    dataArr[i][2],
+    dataArr[i][3],
+    dataArr[i][4],
+    dataArr[i][5],
+    dataArr[i][6],
+    dataArr[i][7],
+    dataArr[i][8],
+    dataArr[i][9],
+    dataArr[i][10],
+    dataArr[i][11],
+    dataArr[i][12],
+    dataArr[i][13]
+
   ));
 }
 
 Compress('Banners');
 Success(count);
-
-
-// test
